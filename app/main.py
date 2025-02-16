8ff3a933ebf9897a8b61a0b25e6a44817a25f4f1
Nikita <nikitaivanov2341@gmail.com>
2025-02-17 02:55:29 +0300

    refactor: добавлен список роутеров

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI
 
 from core.config import APP_HOST, APP_PORT, APP_NAME
-from routers import user_router
+from routers import (
+    user_router,
+    category_router,
+)
+
+
+routers_list = [
+    user_router.router,
+    category_router.router,
+]
 
 
 app = FastAPI(title=APP_NAME)
 
 
-app.include_router(
-    user_router.router,
-)
+for router in routers_list:
+    app.include_router(router)
 
 
 if __name__ == '__main__':
