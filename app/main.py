98ecb1587cbab409726f578681538c85d4276aa0
Nikita <nikitaivanov2341@gmail.com>
2025-02-16 07:57:57 +0300

    fix: изменен порядок путей в `user_router`
    - Обновлен порядок маршрутов в `user_router` для корректной обработки.
    - Заменен ответ эндпоинтов с `UserDTO` на `UserResponse`.
    - Удалены лишние импорты.

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 
 from core.config import APP_HOST, APP_PORT, APP_NAME
+from routers import user_router
 
 
 app = FastAPI(title=APP_NAME)
 
 
 app.include_router(
-    
+    user_router.router,
 )
 
 
--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,5 +1,6 @@
 from typing import List
 from uuid import UUID
+
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import EmailStr
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -7,11 +8,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core.database import get_async_session
 from core.errors.user_error import UserNotFoundError
 from entities.utils import Role
-from models.models import UserModel
-from schemas.user_dto import UserDTO
+from schemas.user_dto import UserResponse
 from services.user_service import AbstractUserService, UserService
 from repositories.user_rep import AbstractUserRepository, UserRepository
-from mappers.entities_dto import user_to_userdto
+from mappers.entities_dto import get_user_response
 
 
 router = APIRouter(prefix='/users', tags=['Users'])
@@ -29,50 +29,50 @@ def get_user_service(
     return UserService(user_rep)
 
 
-@router.get('/{user_id}', response_model=UserDTO)
-async def get_user_by_id(
-    user_id: UUID, user_service: AbstractUserService=Depends(get_user_service)
-) -> UserDTO:
-    try:
-        user = await user_service.get_user_by_id_or_raise(user_id)
-    except UserNotFoundError as e:
-        raise HTTPException(status_code=404, detail=e.message)
-    
-    return user_to_userdto(user)
-
-
-@router.get('/by_email', response_model=UserDTO)
+@router.get('/by_email', response_model=UserResponse)
 async def get_user_by_email(
     email: EmailStr,
     user_service: AbstractUserService=Depends(get_user_service)
-) -> UserDTO:
+) -> UserResponse:
     try:
         user = await user_service.get_user_by_email_or_raise(email)
     except UserNotFoundError as e:
         raise HTTPException(status_code=404, detail=e.message)
     
-    return user_to_userdto(user)
+    return get_user_response(user)
 
 
-@router.get('/list', response_model=List[UserDTO])
+@router.get('/list', response_model=List[UserResponse])
 async def get_users_list(
     user_service: AbstractUserService=Depends(get_user_service)
-) -> List[UserDTO]:
+) -> List[UserResponse]:
     users = await user_service.get_users_list()
-    return [user_to_userdto(user) for user in users]
+    return [get_user_response(user) for user in users]
 
 
-@router.post('/create_user/', response_model=UserDTO)
+@router.get('/{user_id}', response_model=UserResponse)
+async def get_user_by_id(
+    user_id: UUID, user_service: AbstractUserService=Depends(get_user_service)
+) -> UserResponse:
+    try:
+        user = await user_service.get_user_by_id_or_raise(user_id)
+    except UserNotFoundError as e:
+        raise HTTPException(status_code=404, detail=e.message)
+    
+    return get_user_response(user)
+
+
+@router.post('/create_user', response_model=UserResponse)
 async def create_user(
     name: str,
     email: str,
     password: str,
     role: Role=Role.USER,
     user_service: AbstractUserService=Depends(get_user_service)
-) -> UserDTO:
+) -> UserResponse:
     user = await user_service.create_user(name=name, email=email,
                                           password=password, role=role)
-    return user_to_userdto(user)
+    return get_user_response(user)
 
 
 @router.delete('/{user_id}', status_code=204)
